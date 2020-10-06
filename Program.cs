f139f54e12e7de4b764830f236412f8ba8aff764
ffauskanger <33883489+ffauskanger@users.noreply.github.com>
2020-10-06 21:11:31 +0200

    Hello World

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenTK.Windowing.Desktop;
+using System;
+
+namespace Engine
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            GameWindowSettings gameWindowSettings = new GameWindowSettings();
+            NativeWindowSettings nativeWindowSettings = new NativeWindowSettings();
+
+            nativeWindowSettings.Title = "Hello World";
+
+            using (var window = new Window(gameWindowSettings, nativeWindowSettings))
+            {
+                window.Run();
+            }
+        }
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,9 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace Engine
+{
+    public class Window : GameWindow
+    {
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
+    }
+}
