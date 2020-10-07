31bea7d89e4fb955853f6e96b8519be38e4c8d2b
ffauskanger <33883489+ffauskanger@users.noreply.github.com>
2020-10-07 18:03:30 +0200

    Fixed shader

--- Shader.cs
+++ Shader.cs
@@ -12,7 +12,7 @@ namespace Engine
 
         public Shader(string vertexPath, string fragmentPath)
         {
-
+            int _vertexShader, _fragmentShader;
             string _vertexShaderSource, _fragmentShaderSource;
             using (StreamReader reader = new StreamReader(vertexPath, Encoding.UTF8))
             {
@@ -24,7 +24,25 @@ namespace Engine
                 _fragmentShaderSource = reader.ReadToEnd();
             }
 
+            _vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(_vertexShader, _vertexShaderSource);
 
+            _fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(_fragmentShader, _fragmentShaderSource);
+
+            GL.CompileShader(_vertexShader);
+            GL.CompileShader(_fragmentShader);
+
+            _handle = GL.CreateProgram();
+            GL.AttachShader(_handle, _vertexShader);
+            GL.AttachShader(_handle, _fragmentShader);
+
+            GL.LinkProgram(_handle);
+
+            GL.DetachShader(_handle, _vertexShader);
+            GL.DetachShader(_handle, _fragmentShader);
+            GL.DeleteShader(_fragmentShader);
+            GL.DeleteShader(_vertexShader);
         }
 
         protected virtual void Dispose(bool disposing)
@@ -35,7 +53,6 @@ namespace Engine
                 disposedValue = true;
             }
         }
-
 
         ~Shader()
         {
